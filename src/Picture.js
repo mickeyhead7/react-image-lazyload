39ce6b54b601f0874b20ade1ff722ee8fec593a5
Mark Head <mickeyhead7@gmail.com>
2017-10-23 22:19:28 +0100

    Removes blur on lazy load - not quite working

--- src/Picture.js
+++ src/Picture.js
@@ -1,17 +1,44 @@
 import Imgix from 'react-imgix';
+import classNames from 'classnames';
 import React, { Component } from 'react';
 
 export default class Picture extends Component {
+    state = {
+        mounted: false,
+    };
+
+    removeBlur = () => {
+        this.setState({
+            mounted: true,
+        });
+    };
+
     render () {
         const props = this.props;
+        const classes = classNames({
+            image: true,
+            mounted: this.state.mounted,
+        });
 
         return (
-            <Imgix
-                { ...props }
-                type="picture"
-            >
-                {this.props.children}
-            </Imgix>
+            <div className={classes}>
+                <Imgix
+                    { ...props }
+                    type="picture"
+                >
+                    {React.Children.map(this.props.children, child => React.cloneElement(child, {
+                        onMounted: this.removeBlur,
+                    }))}
+                </Imgix>
+                <style jsx>{`
+                    .image {
+                        filter: blur(10px);
+                    }
+                    .mounted {
+                        filter: none;
+                    }
+                `}</style>
+            </div>
         );
     }
 }
